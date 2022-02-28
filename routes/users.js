08aebdf3ecd8861650f0cecd2535389f79870909
julieth724 <91924627+julieth724@users.noreply.github.com>
2022-02-27 20:26:21 -0500

    se inicializa el proyecto

---
+++ routes/users.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express - Hola Angela Users' });
+});
+router.get('/cool', function(req, res, next) {
+  res.render('index', { title: 'Express - Eres genial' });
+});
+
+module.exports = router;
